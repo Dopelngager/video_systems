ad2d50e7caf15c5b5754606b4dfb8eec88b25c6b
Dmitriy <vnukovskiy.dmitriy@gmail.com>
2025-10-07 18:50:53 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/video_systems",
-  assetPrefix: "/video_systems/",
 };
 
 export default nextConfig;
